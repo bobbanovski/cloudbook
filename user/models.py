e839d6168cc1dccc84212b391e07b7095f367c12
bobbanovski <bobbanovski@gmail.com>
2017-02-24 02:29:51 +0000

    create user registration form

--- user/models.py
+++ user/models.py
@@ -2,10 +2,16 @@ from application import db
 from utilities.common import utc_now_ts as now
 
 class User(db.Document): #create object for Mongo
-    username=db.StringField(db_field="u", required = True) #dbfield name in Mongo: single letter to save space
+    username=db.StringField(db_field="u", required = True, unique = True) #dbfield name in Mongo: single letter to save space
     password=db.StringField(db_field="p", required = True)
-    email=db.EmailField(db_field="e", required = True)
+    email=db.EmailField(db_field="e", required = True, unique = True)
     first_name=db.StringField(db_field="fn", max_length=50)
     last_name=db.StringField(db_field="ln", max_length=50)
     created=db.IntField(db_field="c", default=now())
+    bio=db.StringField(db_field="b", max_length=100)
+    
+    meta = {
+        'indexes': ['username', 'email', '-created'] #create indices to sort the data
+    }
+    
     
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from user.forms import RegisterForm
 
 user_app = Blueprint('user_app', __name__) #name of app
 
@@ -6,3 +7,8 @@ user_app = Blueprint('user_app', __name__) #name of app
 @user_app.route('/login')
 def login():
     return "User Login"
+    
+@user_app.route('/register', methods=('GET','POST'))
+def register():
+    form = RegisterForm()
+    return render_template('user/register.html', form=form)
