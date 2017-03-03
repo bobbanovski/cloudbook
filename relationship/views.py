1a3540f0a6bc71b6be28445c7ac35016506e61a3
bobbanovski <bobbanovski@gmail.com>
2017-03-03 02:25:13 +0000

    user relationships added

--- relationship/views.py
+++ relationship/views.py
@@ -1,8 +1,50 @@
-from flask import Blueprint
+from flask import Blueprint, abort, session
 
+from user.models import User
+from user.decorators import login_required
+from relationship.models import Relationship
 
 relationship_app = Blueprint('relationship_app', __name__)
 
-@relationship_app.route('/add_friend', methods=('GET', 'POST'))
-def add_friend():
-    return "Add Friend"
+@relationship_app.route('/add_friend/<to_username>', methods=('GET', 'POST'))
+@login_required
+def add_friend(to_username):
+    logged_user = User.objects.filter(username = session.get('username')).first()
+    to_user = User.objects.filter(username = to_username).first()
+    if to_username:
+        rel = Relationship.get_relationship(logged_user, to_user)
+        if rel == "FRIENDS_PENDING":
+            return rel
+        elif rel == "BLOCKED" :
+            return rel
+        elif rel == "FRIENDS_APPROVED":
+            return rel
+        elif rel == "REVERSE_FRIENDS_PENDING": #Friend has approved
+            Relationship(
+                from_user = logged_user,
+                to_user = to_user,
+                rel_type = Relationship.FRIENDS,
+                status = Relationship.APPROVED
+                ).save()
+            reverse_rel = Relationship.objects.get(
+                from_user = to_user,
+                to_user = logged_user
+                )
+            reverse_rel.status = Relationship.APPROVED
+            reverse_rel.save()
+            return "FRIENDS_APPROVED"
+        elif rel == None:
+            Relationship(
+                from_user = logged_user,
+                to_user = to_user,
+                rel_type = Relationship.FRIENDS,
+                status = Relationship.PENDING
+                ).save()
+            return "FRIENDSHIP_REQUESTED"
+        else:
+            return None
+    else:
+        abort(404)
+            
+            
+            
--- user/views.py
+++ user/views.py
@@ -10,6 +10,8 @@ from user.models import User
 from utilities.common import email
 from settings import UPLOAD_FOLDER
 from utilities.imaging import thumbnail_process
+from relationship.models import Relationship
+from user.decorators import login_required
 
 user_app = Blueprint('user_app', __name__) #name of app
 
@@ -79,15 +81,22 @@ def logout():
 @user_app.route('/<username>')
 def profile(username):
     edit_profile = False
+    rel = None
+    
     user = User.objects.filter(username=username).first()
     if user and session.get('username') and user.username == session.get('username'): #if user is looking at his own profile page
         edit_profile = True
     if user:
-        return render_template('user/profile.html', user = user, edit_profile=edit_profile)
+        # compare relationship between user being looked at and user that is logged in 
+        if session.get('username'): 
+            logged_user = User.objects.filter(username=session.get('username')).first()
+            rel = Relationship.get_relationship(logged_user, user)
+        return render_template('user/profile.html', user = user, rel=rel, edit_profile=edit_profile)
     else:
         abort(404)
     
 @user_app.route('/edit', methods=('GET','POST'))
+@login_required
 def edit():
     error = None
     message = None
